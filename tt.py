9ccac011994b8175bf302892b98a7a67d7b687c9
Sveta <fedsvet3009@mail.ru>
2024-07-12 22:53:21 +0700

    bbb

---
+++ tt.py
@@ -0,0 +1,18 @@
+
+import requests
+import json
+# TOKEN = "7248066777:AAHfKyqOsQQUHys1ZARP2UL4VGqdNM2FhpM"
+# url = f"https://api.telegram.org/bot7248066777:AAHfKyqOsQQUHys1ZARP2UL4VGqdNM2FhpM/sendMessage?chat_id=@Serg1195&text=3"
+# print(requests.get(url).json())
+
+ 
+#def send_msg(text):
+    # token = "7248066777:AAHfKyqOsQQUHys1ZARP2UL4VGqdNM2FhpM"
+    # chat_id = "@Serg1195"
+    # url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
+    # results = requests.get("https://flammlin.com/blog/2022/04/18/python-otpravka-soobshheniya-v-telegram/")
+     
+#print(requests.get("https://api.telegram.org/bot7248066777:AAHfKyqOsQQUHys1ZARP2UL4VGqdNM2FhpM/sendMessage?chat_id=@Serg1195&text=3").json()) 
+results = requests.get("https://flammlin.com/blog/2022/04/18/python-otpravka-soobshheniya-v-telegram/")
+#send_msg("Hello python!")
+print(requests)
